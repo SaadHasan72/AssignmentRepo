3664a53c145bd04f94a8a981e75bf87593ad0ec4
Saad Usama <su656669@gmail.com>
2025-07-07 15:37:55 +0300

    Ass1_Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,7 +10,7 @@ namespace Assignment
         {
 
             #region Q1
-            //////Write a program that allows the user to enter a number then print it.
+            ////Write a program that allows the user to enter a number then print it.
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine(num);
 
@@ -23,10 +23,21 @@ namespace Assignment
                 And mention what will happen */
             string age = "1one";
             int x;
-            bool Age = int.TryParse(age , out x);
-            Console.WriteLine(Age);   
+            bool Age = int.TryParse(age, out x);
+            Console.WriteLine(Age);
             // Thise non valid convertion so when use TryParse Thise Method try to parse string
             // and didn't crush the program
+            #endregion
+
+            #region Q3
+            /* Write C# program that Perform a simple arithmetic operation with floating-point numbers 
+                 And mention what will happen*/
+            int x = 3;
+            float y = 3.3f;
+
+            Console.WriteLine((2*3)+ ++x/5 *6.9 - y--);
+            // first calculation will perform its ++x then (2*3) , x+1 /5 *6.9 , result - y
+            //and in the end of calculatin y will deducted by 1 
             #endregion
 
 
