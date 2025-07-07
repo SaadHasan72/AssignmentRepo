700d8293a3f9f26d1a20a6221c3f0b4753d65905
Saad Usama <su656669@gmail.com>
2025-07-07 15:03:33 +0300

    Ass1_Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1
+            //Write a program that allows the user to enter a number then print it.
+            int Num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Num);
+
+
+            #endregion
+
+            
         }
     }
 }
