cfb0f7ee2f1127fdff7996d66641d5724abe6b8b
Saad Usama <su656669@gmail.com>
2025-07-14 12:52:47 +0300

    Assignment_2 Session_3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,148 +1,148 @@
-﻿using System.ComponentModel;
-using System.Diagnostics.Metrics;
-using System.Drawing;
-using System.Security.Cryptography.X509Certificates;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿//using System.ComponentModel;
+//using System.Diagnostics.Metrics;
+//using System.Drawing;
+//using System.Security.Cryptography.X509Certificates;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Assignment
-{
-    internal class Program
-    {
+//namespace Assignment
+//{
+//    internal class Program
+//    {
         
-        static void Main()
-        {
+//        static void Main()
+//        {
 
-            #region Q1
-            ////Write a program that allows the user to enter a number then print it.
+//            #region Q1
+//            ////Write a program that allows the user to enter a number then print it.
             
-            int num = int.Parse(Console.ReadLine());
-            Console.WriteLine(num);
+//            int num = int.Parse(Console.ReadLine());
+//            Console.WriteLine(num);
 
 
-            #endregion
+//            #endregion
 
-            #region Q2
-            /*Write C# program that converts a string to an integer,
-                but the string contains non-numeric characters.
-                And mention what will happen */
+//            //#region Q2
+//            ///*Write C# program that converts a string to an integer,
+//            //    but the string contains non-numeric characters.
+//            //    And mention what will happen */
 
-            string age = "1one";
-            int x;
-            bool Age = int.TryParse(age, out x);
-            Console.WriteLine(Age);
+//            //string age = "1one";
+//            //int x;
+//            //bool Age = int.TryParse(age, out x);
+//            //Console.WriteLine(Age);
 
-            // Thise non valid convertion so when use TryParse Thise Method try to parse string
-            // and didn't crush the program
-            #endregion
-
-
-            #region Q3
-            /* Write C# program that Perform a simple arithmetic operation with floating-point numbers 
-                 And mention what will happen*/
-
-            int x = 3;
-            float y = 3.3f;
-            Console.WriteLine((2 * 3) + ++x / 5 * 6.9 - y--);
-
-            // first calculation will perform its ++x then (2*3) , x+1 /5 *6.9 , result - y , result + the product of (2*3)
-            //and in the end of calculatin y will deducted by 1 
-            #endregion
-
-            #region Q4
-            //Write C# program that Extract a substring from a given string.  
-
-            string name = "Saad Usama Saad ";
-
-            string FirstName = name.Substring(0, 4);
-            string MedName = name.Substring(5, 5);
-            string LastName = name.Substring(11, 4);
-            Console.WriteLine($"My name is {FirstName} My Father is name {MedName}and Last name is {LastName}");
-            #endregion
-
-            #region Q5
-            //Write C# program that Assigning one value type variable to
-            //another and modifying the value of one variable and mention what will happen
-
-            int a = 10;
-            int b = a;
-
-            Console.WriteLine($"a= {a}");
-            Console.WriteLine($"b={b}");
-
-            a = 20;
-            Console.WriteLine($"a= {a}");
-            Console.WriteLine($"b={b}");
-
-            // after changing value of a the value of b still the same because b gets a copy of a is value
-            // changing a latter dosent effect 
-
-            #endregion
-
-            #region Q6
-
-            //Write C# program that Assigning one reference type variable to another and modifying
-            //the object through one variable and mention what will happen
-
-            Point p01;
-            p01 = new Point();
-            p01.X = 1;
-
-            Point p02 = p01;
-
-            Console.WriteLine($"p01.x ={p01.X}");
-            Console.WriteLine($"p02.x ={p02.X}");
-
-            p02.X = 4;
-
-            Console.WriteLine($"After p01.x ={p01.X}");
-            Console.WriteLine($"After  p02.x ={p02.X}");
+//            //// Thise non valid convertion so when use TryParse Thise Method try to parse string
+//            //// and didn't crush the program
+//            //#endregion
 
 
-            // it the same thing of the previous 
-            #endregion
+//            //#region Q3
+//            ///* Write C# program that Perform a simple arithmetic operation with floating-point numbers 
+//            //     And mention what will happen*/
 
-            #region Q7
-            ////Write C# program that take two string variables and print them as one variable
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            Console.WriteLine($"{x} {y}");
-            #endregion
+//            //int x = 3;
+//            //float y = 3.3f;
+//            //Console.WriteLine((2 * 3) + ++x / 5 * 6.9 - y--);
 
-            #region Q8
-            //Which of the following statements is correct about the C#.NET code snippet given below?
-            int d;
-            d = Convert.ToInt32(!(30 < 20));
+//            //// first calculation will perform its ++x then (2*3) , x+1 /5 *6.9 , result - y , result + the product of (2*3)
+//            ////and in the end of calculatin y will deducted by 1 
+//            //#endregion
 
-            //The code reports an error.
-            #endregion
+//            //#region Q4
+//            ////Write C# program that Extract a substring from a given string.  
 
-            #region Q9
-            //Which of the following is the correct output for the C# code given below?
-            Console.WriteLine(13 / 2 + " " + 13 % 2);
+//            //string name = "Saad Usama Saad ";
 
-            Console.WriteLine(13 / 2 + " " + 13 % 2);
+//            //string FirstName = name.Substring(0, 4);
+//            //string MedName = name.Substring(5, 5);
+//            //string LastName = name.Substring(11, 4);
+//            //Console.WriteLine($"My name is {FirstName} My Father is name {MedName}and Last name is {LastName}");
+//            //#endregion
 
-            A.6.5 1
-            #endregion
+//            //#region Q5
+//            ////Write C# program that Assigning one value type variable to
+//            ////another and modifying the value of one variable and mention what will happen
 
-            #region Q10
+//            //int a = 10;
+//            //int b = a;
+
+//            //Console.WriteLine($"a= {a}");
+//            //Console.WriteLine($"b={b}");
+
+//            //a = 20;
+//            //Console.WriteLine($"a= {a}");
+//            //Console.WriteLine($"b={b}");
+
+//            //// after changing value of a the value of b still the same because b gets a copy of a is value
+//            //// changing a latter dosent effect 
+
+//            //#endregion
+
+//            //#region Q6
+
+//            ////Write C# program that Assigning one reference type variable to another and modifying
+//            ////the object through one variable and mention what will happen
+
+//            //Point p01;
+//            //p01 = new Point();
+//            //p01.X = 1;
+
+//            //Point p02 = p01;
+
+//            //Console.WriteLine($"p01.x ={p01.X}");
+//            //Console.WriteLine($"p02.x ={p02.X}");
+
+//            //p02.X = 4;
+
+//            //Console.WriteLine($"After p01.x ={p01.X}");
+//            //Console.WriteLine($"After  p02.x ={p02.X}");
 
 
-            //10 - What will be the output of the C# code given below?
+//            //// it the same thing of the previous 
+//            //#endregion
+
+//            //#region Q7
+//            //////Write C# program that take two string variables and print them as one variable
+//            //string x = Console.ReadLine();
+//            //string y = Console.ReadLine();
+//            //Console.WriteLine($"{x} {y}");
+//            //#endregion
+
+//            //#region Q8
+//            ////Which of the following statements is correct about the C#.NET code snippet given below?
+//            //int d;
+//            //d = Convert.ToInt32(!(30 < 20));
+
+//            ////The code reports an error.
+//            //#endregion
+
+//            //#region Q9
+//            ////Which of the following is the correct output for the C# code given below?
+//            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+//            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+//            //A.6.5 1
+//            //#endregion
+
+//            //#region Q10
 
 
-            int num = 1, z = 5;
+//            ////10 - What will be the output of the C# code given below?
 
 
-            if (!(num <= 0))
-                Console.WriteLine(++num + z++ + " " + ++z);
-            else
-                Console.WriteLine(--num + z-- + " " + --z);
+//            //int num = 1, z = 5;
 
-            c.6 6
 
-            #endregion
+//            //if (!(num <= 0))
+//            //    Console.WriteLine(++num + z++ + " " + ++z);
+//            //else
+//            //    Console.WriteLine(--num + z-- + " " + --z);
 
-        }
-    }
-}
+//            //c.6 6
+
+//            //#endregion
+
+//        }
+//    }
+//}
---
+++ ass2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace ass2
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            #region Q1
+            ////1 - Write a program that takes a number from the user then print yes
+            ////if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine ("Enter Number");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x % 3 == 0 && x % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            #endregion
+
+            #region Q2
+            ////2- Write a program that allows the user to insert an integer then print negative if it is
+            ////negative number otherwise print positive.
+            //Console.WriteLine("Enter Your num");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x < 0)
+            //{
+            //    Console.WriteLine("Negative");
+
+            //}
+            //else if (x > 0)
+            //{
+            //    Console.WriteLine("Postive");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            #endregion
+            #region Q3
+            //3- Write a program that takes 3 integers from the user then prints the max element and
+            //the min element.
+
+
+            //Console.WriteLine("Enter Your First num");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your secound num");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your Thired num");
+            //int z = int.Parse(Console.ReadLine());
+
+            //if (x > y && x > z)
+            //{
+            //    Console.WriteLine($"Max Element ={x}");
+            //    if (y < z)
+            //    {
+            //        Console.WriteLine($"Min Element = {y}");
+            //    }
+            //    else if (y > z)
+            //    {
+            //        Console.WriteLine($"Min Element = {z}");
+            //    }
+            //}
+            //else if (y > z && y > x)
+            //{
+            //    Console.WriteLine($"Max Element ={y}");
+            //    if (x < z)
+            //    {
+            //        Console.WriteLine($"Min Element = {x}");
+
+            //    }
+            //    else if (z < x)
+            //    {
+            //        Console.WriteLine($"Min Element = {z}");
+            //    }
+            //}
+            //else if (z > y && z > x)
+            //{
+            //    Console.WriteLine($"Max Element ={z}");
+            //    if (x < y)
+            //    {
+            //      Console.WriteLine($"Min Element = {x}");
+            //    }
+            //    else if (y < x)
+            //    {
+            //        Console.WriteLine($"Min Element = {x}");
+            //    }
+            //}
+
+
+
+            #endregion
+
+            #region Q4
+            //// 4 - Write a program that allows the user to insert an integer number then check If a
+            //// number is even or odd.
+            //Console.WriteLine("Enter Your Num");
+            //int x = int. Parse (Console.ReadLine());
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("It is Even");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is Odd");
+
+            //}
+            #endregion
+
+
+            #region Q5
+            ////5- Write a program that takes character from the user then if it is a vowel chars
+            ////(a,e,I,o,u) then print (vowel) otherwise print (consonant).
+
+            //string x = Console.ReadLine();
+            //switch (x)
+            //{
+            //    case  "a"  :
+            //    case  "e"  :
+            //    case  "i"  :
+            //    case  "o"  :
+            //    case  "u"  :
+
+            //        Console.WriteLine("its vowel");
+            //    break;
+            //        default:
+            //        Console.WriteLine("its not vowel");
+            //        break;
+                    
+
+            
+            //}
+
+            #endregion
+        }
+    }
+
+}
